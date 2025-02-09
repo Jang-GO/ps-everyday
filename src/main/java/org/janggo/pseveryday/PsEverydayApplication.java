a652070d7031b4e495b53546df33390d8aad7334
Jang-GO <jang8195@naver.com>
2025-02-09 13:55:25 +0900

    initial commit

---
+++ src/main/java/org/janggo/pseveryday/PsEverydayApplication.java
@@ -0,0 +1,13 @@
+package org.janggo.pseveryday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsEverydayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PsEverydayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/janggo/pseveryday/PsEverydayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.janggo.pseveryday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsEverydayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
