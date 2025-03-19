97056b8910352f8e6e18ef56f4cfc41ba19379ab
Jang-GO <jang8195@naver.com>
2025-03-19 15:48:28 +0900

    Test : 스케쥴러 로직 테스트

--- src/main/java/org/janggo/pseveryday/schedule/ProblemScheduler.java
+++ src/main/java/org/janggo/pseveryday/schedule/ProblemScheduler.java
@@ -1,17 +1,21 @@
 package org.janggo.pseveryday.schedule;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.janggo.pseveryday.mail.MailService;
 import org.janggo.pseveryday.problem.SolvedAcService;
 import org.janggo.pseveryday.problem.dto.SolvedAcResponse;
 import org.janggo.pseveryday.subscriber.Subscriber;
 import org.janggo.pseveryday.subscriber.SubscriberRepository;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
+@Slf4j
+@EnableScheduling
 @RequiredArgsConstructor
 public class ProblemScheduler {
 
@@ -19,13 +23,14 @@ public class ProblemScheduler {
     private final MailService mailService;
     private final SubscriberRepository subscriberRepository;
 
-    @Scheduled(cron = "0 0 8 * * *") // 매일 오전 8시 실행
-    public void scheduleProblemMail() {
+    @Scheduled(cron = "*/10 * * * * *") // 매일 오전 8시 실행
+    public void scheduleRandomProblemMail() {
         List<Subscriber> subscribers = subscriberRepository.findAll();
 
         SolvedAcResponse.Problem randomProblem = solvedAcService.getRandomProblem();
 
         for(Subscriber subscriber: subscribers){
+            log.info("구독지 : {}", subscriber.getEmail());
             mailService.sendProblemMail(subscriber.getEmail(), randomProblem);
         }
     }
