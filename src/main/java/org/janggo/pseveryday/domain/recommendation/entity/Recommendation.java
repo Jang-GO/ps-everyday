d70d0378887ae05cf32398a695be9e5db6817653
Jang-GO <jang8195@naver.com>
2025-04-23 16:38:04 +0900

    feat: 추천내역 엔티티 작성

---
+++ src/main/java/org/janggo/pseveryday/domain/recommendation/entity/Recommendation.java
@@ -0,0 +1,34 @@
+package org.janggo.pseveryday.domain.recommendation.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.janggo.pseveryday.domain.problem.entity.Problem;
+import org.janggo.pseveryday.domain.subscriber.entity.Subscriber;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Recommendation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Subscriber subscriber;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Problem problem;
+
+    private LocalDateTime recommendedAt;
+
+    public Recommendation(Subscriber subscriber, Problem problem) {
+        this.subscriber = subscriber;
+        this.problem = problem;
+        this.recommendedAt = LocalDateTime.now();
+    }
+}
